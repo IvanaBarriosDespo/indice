cafa887fc300827fbea9564e0cf27b564c03840f
IvanaBarriosDespo <128385587+IvanaBarriosDespo@users.noreply.github.com>
2023-03-21 21:44:41 -0300

    Ejercicio Clase 1

---
+++ For (1).java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String args[]) {
+        //Todo Auto-generated method stub
+        System.out.println("Numeros del 1 al 100 (usando FOR): ");
+      int i;
+      for(i=0;i<=100;i++)
+      System.out.println(i); 
+    }
+}

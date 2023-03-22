d6f3231a2b6608d8147853ffac33409d54381405
IvanaBarriosDespo <128385587+IvanaBarriosDespo@users.noreply.github.com>
2023-03-21 21:40:35 -0300

    Add files via upload

---
+++ While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String args[]) {
+        //Todo Auto-generated method stub
+        System.out.println("Numeros del 1 al 100 (usando while): "); //imprimo en pantalla el mensaje
+      int i=1;
+      while(i<=100){
+      System.out.println(i); //se ejecuta el proceso
+      i++;}
+    }
+}

b03bcf0cf9fb09102185135c3eaf9fa07bafe510
IvanaBarriosDespo <128385587+IvanaBarriosDespo@users.noreply.github.com>
2023-03-21 21:48:52 -0300

    Ejercicio Clase 1- Do while

---
+++ While (1).java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String args[]) {
+        //Todo Auto-generated method stub
+        int i=1;
+        System.out.println("Numeros del 1 al 100 (usando Do While): ");
+     do{
+      System.out.println(i); 
+      i++;
+     }while(i<1); //sea cual fuere la condicion, se ejecutara al menos una vez
+    }
+}
